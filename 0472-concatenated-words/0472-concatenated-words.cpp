c404238731638819cb6fa34835015fe34eca8dc4
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-01-27 17:03:14 +0530

    Time: 1119 ms (10.81%), Space: 57.4 MB (45.63%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    map<string, bool> mp;
+    
+    bool can (string s)
+    {
+        if(mp.find(s) != mp.end())
+            return true;
+        int n = s.length();
+        string str = "";
+        for(int i = 0 ; i < n ; i++)
+        {
+            str += s[i];
+            if(mp.find(str) != mp.end())
+            {
+                string next = s.substr(i+1, n - i - 1);
+                if(can(next))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        for(auto i : words)
+            mp[i] = true;
+        vector<string> ans;
+        for(auto i : words)
+        {
+            mp.erase(i);
+            bool ok = can(i);
+            if(ok)
+                ans.push_back(i);
+            mp[i] = true;
+        }
+        return ans;
+    }
+};
