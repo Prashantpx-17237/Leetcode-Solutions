31f293785619147dccf2546d7dee08c1edfe3b4e
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-02-03 03:15:01 +0530

    Time: 15 ms (11.63%), Space: 9.8 MB (25.62%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,13 +1,30 @@
+
+unordered_map<char, int> ump;
+
+bool cmp(string &a , string &b){
+      int n1 = a.size();
+      int n2 = b.size();
+      
+      for(int i = 0; i < min(n1 , n2); i++)
+      {
+          if(ump[a[i]] != ump[b[i]])
+              return ump[a[i]] < ump[b[i]];
+      }
+      return n1 < n2;
+    }
+
 class Solution {
 public:
     
-    bool isAlienSorted(vector<string> words, string order) {
-        int mapping[26];
-        for (int i = 0; i < 26; i++)
-            mapping[order[i] - 'a'] = i;
-        for (string &w : words)
-            for (char &c : w)
-                c = mapping[c - 'a'];
-        return is_sorted(words.begin(), words.end());
+    bool isAlienSorted(vector<string>& words, string order) {
+        //unordered_map<char, int> ump;
+        ump.clear();
+        for(int i = 0 ; i < order.size() ; i++)
+            ump[order[i]] = i;
+        
+        vector<string> nWords = words;
+        sort(words.begin(), words.end(), cmp);
+        //cout << cmp(words[1], words[0]) << endl ;
+        return words == nWords;
     }
 };
