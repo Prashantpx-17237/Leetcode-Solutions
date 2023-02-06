4c3339ea3be455754e8e6129b509029a8d2a2aa4
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-02-06 17:44:39 +0530

    Time: 36 ms (31.71%), Space: 8.3 MB (43.00%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length();
+        vector<vector<bool>> cache(n, vector<bool> (n, false));
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            cache[i][i] = true;
+            int j = 1;
+            
+            while(i - j > -1 and i + j < n)
+            {
+                if(cache[i-j][i+j] == true or s[i-j] != s[i+j])
+                    break;
+                cache[i-j][i+j] = true;
+                j++;
+            }
+            j = i+1;
+            int k = i;
+            while(k > -1 and j < n)
+            {
+                if(s[k] != s[j])
+                    break;
+                cache[k][j] = true;
+                k--, j++;
+            }
+        }
+        int count = 0;
+        
+        for(auto i : cache)
+            for(auto j : i)
+                if(j)
+                    count++;
+        
+        return count;
+    }
+};
