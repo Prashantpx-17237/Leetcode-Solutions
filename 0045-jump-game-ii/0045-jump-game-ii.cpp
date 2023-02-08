9dc7363310e10fde51e75e9e49dda6f8fa2ac970
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-02-08 21:01:43 +0530

    Time: 24 ms (57.69%), Space: 16.7 MB (56.17%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int ans = 1, cur = nums[0], maxReach = nums[0];
+        int n = nums.size();
+        if(n == 1)
+            return 0;
+        for(int i = 1 ; i < n ; i++)
+        {
+            if(cur >= n - 1)
+                return ans;
+            if(i <= cur)
+            {
+                maxReach = max(i + nums[i], maxReach);
+            }
+            else
+            {
+                cur = maxReach;
+                maxReach = (i + nums[i]);
+                ans++;
+            }
+            
+        }
+        if(maxReach >= nums[n-1])
+            return ans;
+        //cout << cur << endl ;
+        return -1;
+    }
+};
