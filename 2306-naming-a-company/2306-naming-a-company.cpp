e4663daad44397ccdc11ed13793623ed9355a2f0
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-02-09 23:37:36 +0530

    Time: 2462 ms (8.13%), Space: 85.7 MB (24.39%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        
+        int n = ideas.size();
+       
+        vector<set<string>> hash(26);
+        
+        // what we need is count of other newS which do not have same start as orginals 
+        // of these newS 
+        
+        for(auto i : ideas)
+        {
+            string cur = i.substr(1);
+            hash[i[0]-'a'].insert(cur);
+        }
+        
+        // hash has all string starting with that char
+        long long ans = 0;
+        
+        for(int i = 0 ; i < 26 ; i++)
+        {
+            for(int j = i + 1 ; j < 26; j++)
+            {
+                long c1 = 0, c2 = 0;
+                for(auto str: hash[i])
+                    if(!hash[j].count(str))
+                        c1++;
+                for(auto str : hash[j])
+                    if(!hash[i].count(str))
+                        c2++;
+                
+                ans += (c1 * c2);
+            }
+        }
+   
+        return ans * 2LL;
+    }
+};
