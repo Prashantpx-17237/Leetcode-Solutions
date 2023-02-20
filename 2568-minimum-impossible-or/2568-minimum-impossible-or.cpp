a8e2905f5a4460d4fc98c9441d0c0fd7d0c73680
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-02-20 21:19:13 +0530

    Time: 296 ms (10.00%), Space: 74.5 MB (40.00%) - LeetHub

---
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minImpossibleOR(vector<int>& nums) {
+        vector<int> bit(31, 0);
+        
+        unordered_set<int> st;
+        
+        for(auto j : nums)
+        {
+            st.insert(j);
+            long int mask = 1;
+            for(int i = 0; i < 31 ; i++)
+            {
+                if((j & mask) == mask)
+                {
+                    bit[i]++;
+                }
+                mask *= 2;
+            }
+        }
+        long long int mask = 1;
+        for(int i = 0 ; i < 31 ; i++)
+        {
+            //cout << bit[i] << " ";
+            if(bit[i] == 0 or !st.count(mask))
+                return mask;
+
+            mask *= 2;
+        }
+        //cout << endl;
+        return mask;
+    }
+};
