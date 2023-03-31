f99185b0d63f29c6b1e597cc23e4b479a980502b
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-03-31 13:15:19 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (9.44%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-     vector<vector<int>> cache;
-    int get(int i, int covered, vector<int>& cost, vector<int>& days){
-        if(i == days.size())
-            return 0;
-        int& ans = cache[i][covered];
-        if(ans != -1)
-            return ans;
-        
-        if(covered >= days[i])
-            return ans = get(i+1, covered, cost, days);
-        
-        int ans1 = cost[0] + get(i+1, days[i], cost, days);
-        int ans2 = cost[1] + get(i+1, days[i] + 6, cost, days);
-        int ans3 = cost[2] + get(i+1, days[i] + 29, cost, days);
-        return ans = min({ans1, ans2, ans3});
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        cache.resize(366, vector<int>(400, -1));
-        return get(0, 0, costs, days);
+        unordered_set<int> has(days.begin(), days.end());
+        vector<int> cache(366, 0);
+        for(int i = 1 ; i < 366 ; i++){
+            if(has.count(i)){
+                cache[i] = min({costs[0] + cache[i-1],
+                                costs[1] + cache[max(0, i-7)],
+                                costs[2] + cache[max(0, i - 30)]});
+            }
+            else cache[i] = cache[i-1];
+        }
+        return cache[365];
     }
 };
