d34d7b912239e324e6ca3e18bf8cf604b57da7fc
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-03-31 12:53:39 +0530

    Time: 46 ms (5.33%), Space: 18.3 MB (5.16%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    int mincostTickets(vector<int>& days, vector<int>& costs, int cost = 0) {
-  queue<pair<int, int>> last7, last30;
-  for (auto d : days) {
-    while (!last7.empty() && last7.front().first + 7 <= d) last7.pop();
-    while (!last30.empty() && last30.front().first + 30 <= d) last30.pop();
-    last7.push({ d, cost + costs[1] });
-    last30.push({ d, cost + costs[2] });
-    cost = min({ cost + costs[0], last7.front().second, last30.front().second });
-  }
-  return cost;
-}
+    map<pair<int, int>, int> cache;
+    int get(int i, int covered, vector<int>& cost, vector<int>& days){
+        if(i == days.size())
+            return 0;
+        
+        if(cache.find({i, covered}) != cache.end())
+            return cache[{i, covered}];
+        
+        if(covered >= days[i])
+            return cache[{i, covered}] = get(i+1, covered, cost, days);
+        
+        int ans1 = cost[0] + get(i+1, days[i], cost, days);
+        int ans2 = cost[1] + get(i+1, days[i] + 6, cost, days);
+        int ans3 = cost[2] + get(i+1, days[i] + 29, cost, days);
+        return cache[{i, covered}] = min({ans1, ans2, ans3});
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n = days.size();
+        return get(0, 0, costs, days);
+    }
 };
