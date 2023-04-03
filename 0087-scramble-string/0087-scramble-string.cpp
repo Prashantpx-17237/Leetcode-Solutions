9ef37ac4a30ac7c509fe6a078e4c0bf66c3c679c
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-04-03 22:38:11 +0530

    Time: 660 ms (5.00%), Space: 39.5 MB (15.20%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
+    map<pair<string, string>, bool> dp;
+    bool can(string& s, string& t){
+       
+       if(s == t) return true;
+        
+        if(dp.find({s,t}) != dp.end())
+            return dp[{s, t}];
+        bool& ans = dp[{s, t}];
+        int n = s.length();
+        
+        for(int i = 0; i < n-1 ; i++){
+            
+            string x = s.substr(0, i+1);
+            string y = s.substr(i+1);
+            string left = t.substr(0, i+1);
+            string right = t.substr(i+1);
+            
+            if((y+x) == t) return ans = true;
+            
+            if(can(x, left) and can(y, right)) return ans = true;
+            int l = x.size(), r = y.size();
+            left = t.substr(0, r), right = t.substr(r);
+            if(can(y, left) and can(x, right)) return ans = true;
+        }
+        return ans = false;
+    }
+    
     bool isScramble(string s1, string s2) {
-        int n = s1.size();
-        vector dp(n + 1, vector(n, vector<int>(n)));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[1][i][j] = s1[i] == s2[j];
-            }
-        }
-        for (int length = 2; length <= n; length++) {
-            for (int i = 0; i < n + 1 - length; i++) {
-                for (int j = 0; j < n + 1 - length; j++) {
-                    for (int newLength = 1; newLength < length; newLength++) {
-                        const vector<int>& dp1 = dp[newLength][i];
-                        const vector<int>& dp2 = dp[length - newLength][i + newLength];
-                        dp[length][i][j] |= dp1[j] && dp2[j + newLength];
-                        dp[length][i][j] |= dp1[j + length - newLength] && dp2[j];
-                    }
-                }
-            }
-        }
-        return dp[n][0][0];
+        return can(s1, s2);
     }
 };
