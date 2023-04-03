98d37deed61277a55cd7bbb5516c61f3406920f0
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-04-03 13:03:27 +0530

    Time: 1347 ms (5.30%), Space: 7.5 MB (95.75%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
@@ -1,34 +1,52 @@
 class Solution {
-    void add(long &a, long &b) { a = (a + b) % ((int) 1e9+7); }
 public:
-    int ways(vector<string>& A, int K) {
-        int M = A.size(), N = A[0].size();
-        vector<vector<int>> cnt(M + 1, vector<int>(N + 1));
-        for (int i = M - 1; i >= 0; --i) {
-            int s = 0;
-            for (int j = N - 1; j >= 0; --j) {
-                s += A[i][j] == 'A';
-                cnt[i][j] = cnt[i + 1][j] + s;
+    
+    // agar vertical se kata to left wala gya 
+    // else horizontal case me uppar wala gya
+    int dp[51][51][10];
+    const int mod = 1E9 + 7;
+    int canCut(vector<string>& pizza, int x1, int x2, int y1, int y2){
+        int count = 0;
+        for(int i = x1 ; i < x2 ; i++){
+            for(int j = y1 ; j < y2; j++)
+                count += (pizza[i][j] == 'A');
+        }
+        return count;
+    }
+    int get(vector<string>& pizza, int x1, int y1, int x2, int y2, int rem, int tot)
+    {
+        int& cache = dp[x1][y1][rem];
+        if(tot < rem){
+            return cache=0;
+        }
+        if(rem == 1){
+           
+            return cache = 1;
+        }
+        if(cache != -1)
+            return cache;
+        int ans = 0;
+        for(int i = x1 + 1; i <= x2 ; i++){
+            int val = canCut(pizza, x1, i, y1, y2+1);
+            if(val > 0) {
+                ans = ans % mod + get(pizza, i, y1, x2, y2, rem - 1, tot-val) % mod;
             }
         }
-        vector<vector<vector<long>>> dp(M + 1, vector<vector<long>>(N + 1, vector<long>(K + 1)));
-        for (int i = M - 1; i >= 0; --i) {
-            for (int j = N - 1; j >= 0; --j) {
-                dp[i][j][1] = cnt[i][j] > 0;
-                for (int k = 2; k <= K; ++k) {
-                    for (int t = i + 1; t < M; ++t) {
-                        if (cnt[i][j] == cnt[t][j]) continue;
-                        if (cnt[t][j] == 0) break;
-                        add(dp[i][j][k], dp[t][j][k - 1]);
-                    }
-                    for (int t = j + 1; t < N; ++t) {
-                        if (cnt[i][j] == cnt[i][t]) continue;
-                        if (cnt[i][t] == 0) break;
-                        add(dp[i][j][k], dp[i][t][k - 1]);
-                    }
-                }
+        for(int j = y1 + 1; j <= y2; j++){
+            int val = canCut(pizza, x1, x2+1, y1, j);
+            if(val > 0){
+                ans = ans % mod + get(pizza, x1, j, x2, y2, rem - 1, tot-val) % mod;
             }
         }
-        return dp[0][0][K];
+        ans = ans % mod;
+        return cache = ans;
+     }
+    
+    int ways(vector<string>& pizza, int k) {
+        int n = pizza.size(), m = pizza[0].length();
+        memset(dp, -1, sizeof(dp));
+        int count = 0;
+        for(auto i : pizza) for(auto j : i) count += (j == 'A');
+        return get(pizza, 0, 0, n-1, m-1, k, count);
     }
 };
