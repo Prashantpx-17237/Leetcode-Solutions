a7b702b4a377ec2aa2694b209bd2fccd70a57023
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-04-09 15:23:12 +0530

    Time: 705 ms (38.46%), Space: 217.6 MB (17.09%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -2,44 +2,40 @@ class Solution {
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n = colors.length();
-        vector<int> in(n, 0);
+        
         vector<vector<int>> adj(n);
         
         for(auto edge : edges){
             adj[edge[0]].push_back(edge[1]);
-            in[edge[1]]++;
+        }
+        vector<int> vis(n, 0);
+        vector<vector<int>> count(n, vector<int>(26, 0));
+        int ans = -1;
+        for(int i = 0 ; i < n ; i++){
+            ans = max(ans, dfs(adj, colors, i, vis, count));
         }
         
-        int ans = 0;         
-        vector<int> tsort = topo(in, adj, ans, colors);
-        if(tsort.size() != n) return -1;
-        
-        return ans;
+        return ans == INT_MAX ? -1 : ans;
     }
 private:
-    vector<int> topo(vector<int>& in, vector<vector<int>>& adj, int& ret, string& colors){
-        vector<vector<int>> col(colors.length(), vector<int>(26, 0));
-        queue<int> q;
-        for(int i = 0 ; i < in.size() ; i++){
-            if(in[i] == 0)
-                q.push(i);
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            int u = q.front(); q.pop();
-            ret = max(ret, ++col[u][colors[u]-'a']);
-            //cout << col[u][colors[u]-'a'] << " ";
-            ans.push_back(u);
-            for(int v : adj[u]){
-                for(int i = 0 ; i < 26 ; i++){
-                    col[v][i] = max(col[v][i], col[u][i]);
-                }
-                //cout << "v = " << v << " in[v] = " << in[v] << endl ;
-                in[v]--;
-                if(in[v] == 0) q.push(v);
+    int dfs(vector<vector<int>>& adj, string& colors, int u, vector<int>& vis, vector<vector<int>>& count){
+        if(vis[u] == 1)
+            return INT_MAX;
+        if(vis[u] == 2) return count[u][colors[u]-'a'];
+        
+        vis[u] = 1;
+        
+        for(auto v : adj[u]){
+            int val = dfs(adj, colors,  v, vis, count);
+            //cout << "val = " << val << endl;
+            if(val == INT_MAX) return INT_MAX;
+            for(int i = 0 ; i < 26 ; i++){
+                count[u][i] = max(count[u][i], count[v][i]);
             }
         }
-        return ans;
+        count[u][colors[u]-'a']++;
+        vis[u] = 2;
+        return count[u][colors[u]-'a'];
     }
     
 };
