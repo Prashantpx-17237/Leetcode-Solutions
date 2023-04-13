80fde57c18199fbd3bd7efffdcead6b7ab556075
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-04-13 22:20:28 +0530

    Time: 13 ms (33.03%), Space: 15.4 MB (68.46%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -5,21 +5,13 @@ public:
         int n = size(pushed);
         int i = 0, j = 0;
         while(i < n){
+            st.push(pushed[i]);
             while(!st.empty() and st.top() == popped[j]){
                 st.pop();
                 j++;
             }
-            if(popped[j] != pushed[i]) 
-                st.push(pushed[i]);
-            else
-                j++;
             i++;
         }
-        while(!st.empty() and st.top() == popped[j]){
-                st.pop();
-                j++;
-            }
-        cout << j << endl ;
         return j == n;
     }
 };
