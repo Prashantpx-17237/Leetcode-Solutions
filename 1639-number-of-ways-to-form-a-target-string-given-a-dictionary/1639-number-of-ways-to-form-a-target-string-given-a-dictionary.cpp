98b8524a6b60761ef27f3a4c16011239f3edf967
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-04-16 21:15:04 +0530

    Time: 327 ms (49.57%), Space: 54.5 MB (42.67%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -6,16 +6,30 @@ public:
         
         int mod = 1E9 + 7;
         
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < words.size(); j++){
-                count[i][words[j][i] -'a']++;
+        for(int i = 0 ; i < words.size() ; i++){
+            
+            for(int j = 0 ; j < m; j++){
+                
+                count[j][words[i][j] -'a']++;
             }
         }
+        
+        // count array stores count of any character ch on ith index        
+        
         for(int i = 0 ; i <= m ; i++) dp[0][i] = 1;
+        
         for(int i = 1 ; i <= n ; i++){
+            
             for(int j = 1 ; j <= m ; j++){
+                
                 dp[i][j] = ((dp[i-1][j-1] * 1LL * count[j-1][target[i-1]-'a']) % mod + 
                     dp[i][j-1]) % mod;
+                
+                // dp[i][j] represents number of ways we can make i len target string
+                //          using upto j indices of all strings in words. 
+                
+                // when we make ith char => number of ways to (i-1, j-1) * count(j-1, cur char)
+                // when ith character is already made using j-1 charac only then dp[i][j-1]
                 
             }
         }
