accd763a9e51cc58327937c389ec4cd08d507fd6
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-04-23 11:08:43 +0530

    Time: 58 ms (77.91%), Space: 11.9 MB (76.74%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     const int mod = 1E9+7;
-    int get(string& s, int ind, int k, vector<int>& dp){
+    /*int get(string& s, int ind, int k, vector<int>& dp){
         if(ind == s.length())
             return 1;
         if(s[ind] == '0') return 0;
@@ -16,12 +16,25 @@ public:
                 break;
         }
         return dp[ind] = ans % mod;
-    }
+    }*/
     
     int numberOfArrays(string s, int k) {
         
         int n = s.length();
-        vector<int> dp(n, -1);
-        return get(s, 0, k, dp);
+        vector<int> dp(n, 0);
+        
+        for(int i = n - 1 ; i > -1 ; i--){
+            if(s[i] == '0') continue;
+            long int cur = 0;
+            for(int j = i ; j < n ; j++){
+                cur = cur * 10 + (s[j]-'0');
+                if(cur <= k){
+                    dp[i] = (dp[i] + (j+1 < n ? dp[j+1] : 1))% mod;
+                }
+                else break;
+            }
+        }
+        
+        return dp[0];
     }
 };
