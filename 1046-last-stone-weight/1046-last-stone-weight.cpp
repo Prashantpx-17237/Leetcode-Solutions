1bad7d2dc79bd45a0cceb62f8d5452ff1f61b9fe
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-04-25 00:17:44 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (41.71%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -5,10 +5,8 @@ public:
         for(auto i : stones) q.push(i);
         
         while(q.size() > 1){
-            int x = q.top(); q.pop();
-            int y = q.top() ; q.pop();
-            if(x == y) continue;
-            if(x > y) swap(x, y);
+            int y = q.top(); q.pop();
+            int x = q.top() ; q.pop();
             q.push(y-x);
         }
         
