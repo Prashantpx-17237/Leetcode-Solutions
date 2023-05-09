d03432ef5cec0bd4c7919caf25146fc47cab1ab6
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-05-09 08:08:27 +0530

    Time: 4 ms (33.67%), Space: 6.8 MB (69.57%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int x1 = 0, x2 = matrix.size() - 1, y1 = 0, y2 = matrix[0].size()-1;
+        
+        vector<int> ans;
+        
+        while(x1 <= x2 and y1 <= y2){
+           // cout << "x1 = " << x1 << " x2 = " << x2 << " y1 = " << y1 << " y2 = " << y2 << endl;
+            for(int i = y1 ; i <= y2 ; i++)
+                ans.push_back(matrix[x1][i]);
+            for(int i = x1 + 1 ; i <= x2 ; i++)
+                ans.push_back(matrix[i][y2]);
+            for(int i = y2 - 1 ; i >= y1 && (x2 - x1) > 0 ; i--)
+                ans.push_back(matrix[x2][i]);
+            for(int i = x2-1 ; i > x1 && (y2 - y1) > 0 ; i--)
+                ans.push_back(matrix[i][y1]);
+            
+            x1++, y1++, x2--, y2--;
+        }
+        
+        return ans;
+    }
+};
