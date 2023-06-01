9ff8b0e72d562745061fe14e469ddba5f707e4f6
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-06-01 21:17:02 +0530

    Time: 80 ms (53.71%), Space: 20.9 MB (61.94%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void print(queue<pair<int, int>> q){
+        while(!q.empty()){
+            cout <<  q.front().first <<" = x, y = "<< q.front().second << " / " << endl;
+            q.pop();
+        }
+    }
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(grid[0][0] == 1) return -1;
+        vector<vector<int>> vis(n, vector<int>(n, -1));
+        
+    int dir[2][8] = {{0, 0, 1, 1, 1, -1, -1, -1}, 
+                     {1, -1, 0, 1, -1, 0, 1, -1}};
+        
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        vis[0][0] = 1;
+        
+        while(!q.empty()){
+            int x = q.front().first, y = q.front().second;
+            q.pop();
+            
+            for(int i = 0 ; i < 8; i++){
+                int nx = x+dir[0][i], ny = y+dir[1][i];
+                if((nx < n and nx > -1 and ny > -1 and ny < n) and vis[nx][ny] == -1 and grid[nx][ny] == 0){
+                    q.push({nx, ny});
+                    vis[nx][ny] = vis[x][y]+1;
+                }
+            }
+        }
+        return vis[n-1][n-1];
+    }
+};
