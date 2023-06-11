8014a303896d9bd55c0095f42a73567f91de7c68
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-06-11 19:41:23 +0530

    Time: 383 ms (74.82%), Space: 145.6 MB (47.76%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.cpp
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -1,46 +1,25 @@
 class SnapshotArray {
 public:
-    int snapid = 0;
-    map<pair<int, int>, int> data; // key, ind, val
-    map<int, vector<int>> lastUpd;
+    int snapId;
+    vector<vector<pair<int, int>>> historyRecords;
     SnapshotArray(int length) {
-        for(int i = 0 ; i < length; i++) {
-            data[{snapid, i}]  = 0; 
-            lastUpd[i].push_back(snapid);
+        snapId = 0;
+        historyRecords.resize(length);
+        for (int i = 0; i < length; ++i) {
+            historyRecords[i].push_back(make_pair(0, 0));
         }
     }
     
     void set(int index, int val) {
-        data[{snapid, index}] = val;
-        
-        if(lastUpd[index].back() != snapid)
-            lastUpd[index].push_back(snapid);
+        historyRecords[index].push_back(make_pair(snapId, val));
     }
     
     int snap() {
-        return snapid++;
+        return snapId++;
     }
     
     int get(int index, int snap_id) {
-        if(data.find({snap_id, index}) != data.end())
-            return data[{snap_id, index}];
-        int ind = get_snapid(snap_id, index);
-        //cout << data[{ind, index}] << endl;
-        return data[{ind, index}];
-    }
-    
-    int get_snapid(int snap_id, int index){
-        //cout << "called at " << snap_id << " index = " << index << endl ;
-        int ind = lower_bound(lastUpd[index].begin(), lastUpd[index].end(), snap_id) - lastUpd[index].begin();
-        //cout << ind << endl ;
-        return lastUpd[index][ind-1];
+        auto it = upper_bound(historyRecords[index].begin(), historyRecords[index].end(), make_pair(snap_id, INT_MAX));
+        return prev(it)->second;
     }
 };
-
-/**
- * Your SnapshotArray object will be instantiated and called as such:
- * SnapshotArray* obj = new SnapshotArray(length);
- * obj->set(index,val);
- * int param_2 = obj->snap();
- * int param_3 = obj->get(index,snap_id);
- */
