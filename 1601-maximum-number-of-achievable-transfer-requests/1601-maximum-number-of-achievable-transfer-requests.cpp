a3aa94eb1b9779092e5f9d30a12582c1f5289fd2
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-07-02 09:06:21 +0530

    Time: 929 ms (22.55%), Space: 182.9 MB (28.43%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        int m = requests.size();
+        int ans = 0;
+        for(int i = 0 ; i < (1 << m) ; i++){
+            vector<int> count(n, 0);
+            for(int j = 0 ; j < m; j++){
+                if(i & (1 << j)){
+                    count[requests[j][0]]--;
+                    count[requests[j][1]]++;
+                }
+            }
+            int ok = 0;
+            for(int j = 0; j < n; j++) ok += (count[j] != 0);
+            if(ok == 0) ans = max(ans, __builtin_popcount(i));
+        }
+        return ans;
+    }
+};
