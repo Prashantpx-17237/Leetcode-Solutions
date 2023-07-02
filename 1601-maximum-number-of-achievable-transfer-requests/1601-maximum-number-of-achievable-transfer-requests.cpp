be236a9306ef878ae3a8ed899d4545463b9df4df
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-07-02 09:13:48 +0530

    Time: 635 ms (33.33%), Space: 182.7 MB (33.33%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -5,6 +5,8 @@ public:
         int ans = 0;
         for(int i = 0 ; i < (1 << m) ; i++){
             vector<int> count(n, 0);
+            int bitc = __builtin_popcount(i);
+            if(bitc <= ans) continue;
             for(int j = 0 ; j < m; j++){
                 if(i & (1 << j)){
                     count[requests[j][0]]--;
@@ -13,7 +15,7 @@ public:
             }
             int ok = 0;
             for(int j = 0; j < n; j++) ok += (count[j] != 0);
-            if(ok == 0) ans = max(ans, __builtin_popcount(i));
+            if(ok == 0) ans = max(ans, bitc);
         }
         return ans;
     }
