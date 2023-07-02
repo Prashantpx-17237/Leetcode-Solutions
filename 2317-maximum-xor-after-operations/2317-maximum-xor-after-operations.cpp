0893eb9acba27ed273285b413102bd63f1353fa1
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-07-02 09:34:54 +0530

    Time: 106 ms (55.81%), Space: 47.9 MB (99.48%) - LeetHub

--- 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maximumXOR(vector<int>& nums) {
-        vector<int> bit(32, 0);
         
-        for(int i = 0 ; i < nums.size() ; i++){
-            for(int j = 0 ; j < 32; j++){
-                if(nums[i] & (1 << j)) bit[j]++;
-            }
-        }
+        /*
+           nums[i] XOR x means we can flip any bit
+           nums[i] AND nums[i] XOR x means removing bit from nums[i]
+           
+        */
+        
         int ans = 0;
-        for(int i = 0 ; i < 32 ; i++){
-            if(bit[i] > 0) ans += (1 << i);
-        }
-        
+        for(auto i : nums) ans |= i;        
         return ans;
     }
 };
