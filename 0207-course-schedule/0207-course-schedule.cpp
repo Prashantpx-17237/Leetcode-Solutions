499493eb870cc529f65361b3bea0039f6c0cbd27
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-07-13 08:51:09 +0530

    Time: 22 ms (77.19%), Space: 13.8 MB (52.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses], indegree(numCourses, 0);
-        
-        for(auto i : prerequisites){
-            adj[i[1]].push_back(i[0]);
-            indegree[i[0]]++;
+    bool canFinish(int n, vector<vector<int>>& adj) {
+        vector<int> v(n, 0) ;
+        vector<vector<int>> a(n) ;
+        for(int i = 0 ; i < adj.size() ; i++)
+        {
+            a[adj[i][0]].push_back(adj[i][1]) ;
+            //v[adj[i][1]] = -1 ;
         }
-        
-        queue<int> q;        
-        for(int i = 0 ; i < numCourses; i++) if(indegree[i] == 0) q.push(i);
-        int count = q.size();
-        
-        while(!q.empty()){
-            int tp = q.front();
-            q.pop();
-            for(auto u : adj[tp]){
-                indegree[u]--;
-                if(indegree[u] == 0) {
-                    q.push(u);
-                    count++;
-                }
+        for(int i = 0 ; i < n ; i++)
+            if(v[i] == 0 and dfs(i, a, v))
+                return false ;
+        return true ;
+    }
+    bool dfs(int s, vector<vector<int>>& adj, vector<int>& v)
+    {
+        v[s] = 1 ;
+        for(int u : adj[s])
+        {
+            if(v[u] == 0)
+            {
+                if(dfs(u, adj, v))
+                    return true ;
             }
+            else if(v[u] == 1)
+                return true ;
         }
-        return count == numCourses;
+        v[s] = 2 ;
+        return false ;
     }
 };
