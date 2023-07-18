cc6452d8404a2f60614180b0f96afbc3c735e056
Prashant Kumar <72535527+Prashantpx-17237@users.noreply.github.com>
2023-07-18 19:11:10 +0530

    Time: 653 ms (5.00%), Space: 177.9 MB (37.58%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,45 @@
+class LRUCache {
+public:
+    map<int, pair<int, int>> mp;
+    int time, cap;
+    // time, key
+    // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
+    set<pair<int, int>> st;
+    LRUCache(int capacity) {
+        time = 0;
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        if(mp.find(key) == mp.end()) return -1;
+        auto it = st.erase({mp[key].second, key});
+        mp[key] = {mp[key].first, ++time};
+        st.insert({mp[key].second, key});
+        return mp[key].first;
+    }
+    
+    void put(int key, int value) {
+        int sz = st.size();
+        if(mp.find(key) != mp.end()){
+            auto it = st.erase({mp[key].second, key});
+            mp[key] = {value, ++time};
+            st.insert({mp[key].second, key});
+        }
+        else{
+            if(sz == cap){
+                pair<int, int> ele = *st.begin();
+                st.erase(st.begin());
+                mp.erase(ele.second);
+            }
+            mp[key] = {value, ++time};
+            st.insert({time, key});
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
